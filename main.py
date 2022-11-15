b3d35609c18860da4ec1f951fbc7be1fb02729b7
FelipeArredond <pipechiqui77@gmail.com>
2022-11-15 18:51:13 -0500

    Implemented serial endpoints

--- main.py
+++ main.py
@@ -4,6 +4,23 @@ import serial
 
 app = Flask(__name__)
 
+ser = serial.Serial('/dev/ttyACM0',9600)
+ser.flushInput()
+
+@app.route('/led/encender',methods=['POST'])
+def save_encenderLed():
+    data = "encendido"
+    message = data.encode('latin-1')
+    ser.write(message)
+    response = {'estadoLed': 'encendido'}
+    return jsonify(response)
+@app.route('/led/apagar',methods=['GET'])
+def get_apagarLed():
+    data="apagado"
+    message=data.encode('latin-1')
+    ser.write(message)
+    response = {'estadoLed': 'apagado'}
+    return jsonify(response)
 
 if __name__ == '__main__':
     app.run(debug=True)
